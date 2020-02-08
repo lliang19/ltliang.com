69eaa92b7606e64abced2fd8157727c4a866ac95
Lee Liang <ltliang1@gmail.com>
2020-02-07 22:12:21 -0800

    remove web export

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import { SERVER_PORT } from './config';
 import * as web from './web';
 
 async function main() {
-  if (process.env.DEV === 'true') {
-    await web.dev_start(SERVER_PORT);
-    console.log(`Server started at http://localhost:${SERVER_PORT}`);
-  } else {
-    await web.start(SERVER_PORT);
-  }
+  // if (process.env.DEV === 'true') {
+  //   await web.dev_start(SERVER_PORT);
+  //   console.log(`Server started at http://localhost:${SERVER_PORT}`);
+  // } else {
+  await web.start(SERVER_PORT);
+  // }
 }
 
 main().catch(error => console.error(error));
