a11ac2cfc44e0d538e423e17e33839f5d1cb897e
Lee Liang <ltliang1@gmail.com>
2020-02-21 12:02:13 -0800

    add wakeUpDyno

--- src/web/prod.ts
+++ src/web/prod.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import http from 'http';
 import path from 'path';
+import wakeUpDyno from './wakeUpDyno';
 
 // Express app initialization
 const app = express();
+const DYNO_URL = 'http://www.ltliang.com';
 
 // Template configuration
 app.set('view engine', 'ejs');
@@ -30,7 +32,10 @@ const start = (port: number): Promise<void> => {
 
   // eslint-disable-next-line
   return new Promise<void>((resolve, reject) => {
-    server.listen(port, resolve);
+    server.listen(port, () => {
+      wakeUpDyno(DYNO_URL, 25, () => null);
+      resolve();
+    });
   });
 };
 
